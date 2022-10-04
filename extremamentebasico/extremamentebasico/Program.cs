40975fcf5adec3b2f0c96cb0e27ab8ea0347a86e
fernandpaiva <fernandopaiva1705@gmail.com>
2022-10-03 23:11:18 -0300

    EXC2

---
+++ extremamentebasico/extremamentebasico/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace extremamentebasico
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = int.Parse(Console.ReadLine());
+            int B = int.Parse(Console.ReadLine());
+            int X = (A + B);
+            Console.WriteLine("X = " + X);
+
+        }
+    }
+}
