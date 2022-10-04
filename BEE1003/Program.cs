02debcde3c0c4ef1a62dd9d98f4cb9a580661c86
fernandpaiva <fernandopaiva1705@gmail.com>
2022-10-03 23:34:57 -0300

    1003

---
+++ BEE1003/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace BEE1003
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = int.Parse(Console.ReadLine());
+            int B = int.Parse(Console.ReadLine());
+            int Soma = A + B;
+
+            Console.WriteLine("SOMA = " + Soma);
+        }
+    }
+}
